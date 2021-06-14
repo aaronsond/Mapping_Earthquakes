0212ec3ac849852ff009b0d8dfef698a27e40ee2
David Aaronson <aaronsond@gmail.com>
2021-06-14 03:24:13 -0700

    13.6.6

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -36,7 +36,7 @@ console.log("working");
 
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -52,23 +52,27 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Streets": streets,
-  "Satellite Streets": satelliteStreets
+  "Satellite": satelliteStreets
 };
 // Create the map object with center and zoom level.
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
-  layers: [satelliteStreets]
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
 })
 //.setView([30, 30], 3);
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
 // let torontoData = "https://raw.githubusercontent.com/aaronsond/Mapping_Earthquakes/main/torontoRoutes.json";
 // Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/aaronsond/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+// let torontoHoods = "https://raw.githubusercontent.com/aaronsond/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
 
 
@@ -78,10 +82,10 @@ let myStyle = {
   weight: 2
 }
 
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-  L.geoJSON(data).addTo(map);
-})
+// d3.json(torontoHoods).then(function(data) {
+//   console.log(data);
+//   L.geoJSON(data).addTo(map);
+// })
 // // Grabbing our GeoJSON data.
 // d3.json(torontoData).then(function(data) {
 //   console.log(data);
